d72b5cd6c91cc8cf523fb5fd338103e7619fa680
Niraj Patel <105034867+niraj100-code@users.noreply.github.com>
2026-07-01 12:50:51 +0530

    Add files via upload

---
+++ ReactJS-Assignment-main/Module-9) React - Components, State, Props/LAB PRACTICE TASK/Task3_Functional_Component/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="priyanka" />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS-Assignment-main/Module-9) React - Components, State, Props/LAB PRACTICE TASK/Task3_Functional_Component/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // If you have any global styles
+import App from './App';  // Assuming you have an App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
