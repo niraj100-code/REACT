bee42a90ed268e6307f237633a0baab86c6d093a
Niraj Patel <105034867+niraj100-code@users.noreply.github.com>
2026-07-01 12:55:13 +0530

    Add files via upload

---
+++ Assignment/Module-React_Components_State_Props/LAB PRACTICE TASK/Task5_UserCard_Component/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import UserCard from './UserCard';
+
+function App() {
+  return (
+    <div>
+      <UserCard name="Vrishank" age={3} location="India" />
+    </div>
+  );
+}
+
+export default App;
